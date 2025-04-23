3837d360df9e63e57ce716c3080edaf922be164b
Guillermo Garcia <guillermogarcia96331@gmail.com>
2025-04-22 18:51:12 -0700

    started converting to c++

--- P1/main_basic.py
+++ P1/main_basic.py
@@ -3,3 +3,26 @@ from list import *
 from process import Process
 
 ready_list = LinkedList()
+
+"""
+
+PCB Data Structure, which is implemented in my program
+as a standard python list of "Process" objects, where each of these
+process objects contains the process's state, parent index, 
+list of children, and list of resources.
+
+"""
+PCB = []
+
+def create(i : int):
+    """
+    TODO: 
+    1. Allocate new PCB
+    2. set its state to ready.
+    3. insert it into its parent's children list
+    4. set its parent
+    5. intitialize its children list
+    6. initialize its resources list
+    7. Insert into ready list
+    8. display "Process j Created"
+    """
--- P1/process.py
+++ P1/process.py
@@ -1,19 +1,5 @@
 class Process():
-    def __init__(self, parent_num : int, index : int):
+    def __init__(self, parent_num : int):
         self.parent = parent_num
-        self.index = index
         self.state = 1 # 1 indicates it is in the Ready state, whereas 0 indiicates it is blocked
         pass
-
-    def create():
-        """
-        TODO: 
-        1. Allocate new PCB (create module with PCB Class)
-        2. set its state to ready.
-        3. insert it into its parent's children list
-        4. set its parent
-        5. intitialize its children list
-        6. initialize its resources list
-        7. Insert into ready list
-        8. display "Process j Created"
-        """
