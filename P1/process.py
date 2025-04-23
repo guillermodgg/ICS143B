cdff9fa1603c2bf805e012bbbb599201c3ac4df2
Guillermo Garcia <guillermogarcia96331@gmail.com>
2025-04-23 15:31:18 -0700

    removed pythonic versions

--- P1/process.py
+++
@@ -1,5 +0,0 @@
-class Process():
-    def __init__(self, parent_num : int):
-        self.parent = parent_num
-        self.state = 1 # 1 indicates it is in the Ready state, whereas 0 indiicates it is blocked
-        pass
