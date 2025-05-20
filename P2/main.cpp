0d69eb532dc8840f8e44bf583e18ab8d990c02db
Guillermo Garcia <guillermogarcia96331@gmail.com>
2025-05-20 15:41:28 -0700

    starts parsing first line of init file

--- P2/main.cpp
+++ P2/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -12,18 +13,39 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // open the initialization file
     ifstream init_file(argv[1]);
 
+    // check if the file was opened correctly
     if (!init_file.is_open()) {
         cerr << "Could not open initialization file \"" << argv[1] << "\"" << endl;
         return 1;
     }
 
+    // open the input file
     ifstream input_file(argv[2]);
 
+    // check if the file was opened correctly
     if (!input_file.is_open()) {
         cerr << "Could not open input file \"" << argv[2] << "\"" << endl;
         return 1;
+    }
+
+    // initialize PM
+
+    string line;
+
+    // pass through the first line of init file
+    getline(init_file, line);
+    istringstream iss(line);
+    string s, z, f;
+
+    //read each triplet and assign its respective values to the ST
+    while (iss >> s >> z >> f) {
+        // set segment size
+        PM[2 * stoi(s)] = stoi(z);
+        // set frame number (positive or negative)
+        PM[2 * stoi(s) + 1] = stoi(f);
     }
 
     return 0;
